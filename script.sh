f0d208807008e2e59f390ab6f41d6c928dcdb210
Ruben Luyten <45121406+dydus@users.noreply.github.com>
2021-04-12 09:33:31 +0200

    updated script

--- script.sh
+++ script.sh
@@ -1,13 +1,12 @@
-  #!/bin/bash
-  apt-get -y update
+#!/bin/bash
+apt-get -y update
 
-  # set up a silent install of MySQL
-  dbpass=$1
-  # install the LAMP stack
-  apt-get -y install php
-  apt-get -y install apache2
-  # write some PHP
-  echo \My Demo for Koen and Karel\ > /var/www/html/index.php
+# set up a silent install of MySQL
+dbpass=$1
+# install the LAMP stack
+apt-get -y install apache2 mysql-server php5 php5-mysql
+# write some PHP
+echo \My Demo for Koen and Karel\ > /var/www/html/index.php
 
-  # restart Apache
-  apachectl restart
+# restart Apache
+apachectl restart
